ab927f1c3334f031282f315ee87cc2b416ddb7ed
Romnick Sarmiento <135184315+s4rmier@users.noreply.github.com>
2023-09-26 07:34:54 +0800

    draft/archived -ok

--- posts/urls.py
+++ posts/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("posts/", views.PostListView.as_view(), name="list"),
     path("<int:pk>/", views.PostDetailView.as_view(), name="detail"),   
     path("<int:pk>/update/", views.PostUpdateView.as_view(), name="update"),
-    path("<int:pk>/delete/", views.PostDeleteView.as_view(), name="delete"),   
+    path("<int:pk>/delete/", views.PostDeleteView.as_view(), name="delete"),
+    path("draft/", views.DraftPostListView.as_view(), name="draft"),
+    path("archived/", views.ArchivedPostListView.as_view(), name="archived"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -6,43 +6,97 @@ from django.views.generic import (
     DeleteView
 )
 from django.urls import reverse_lazy
-from .models import Post
+from .models import Post, Status
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
+from django.core.exceptions import PermissionDenied
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     template_name = "posts/new.html"
     model = Post
-    fields = ["title", "author", "subtitle", "body"]
+    fields = ["title", "subtitle", "body", "status"]
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostUpdateView(
+    LoginRequiredMixin, 
+    UserPassesTestMixin, 
+    UpdateView):
+    template_name = "posts/update.html"
+    model = Post
+    fields = ["title", "subtitle", "body", "status"]
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(
+    LoginRequiredMixin,
+    UserPassesTestMixin,
+    DeleteView):
+    template_name = "posts/delete.html"
+    model = Post
+    success_url = reverse_lazy("list")
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+    
 class PostListView(ListView):
     template_name = "posts/list.html"
     model = Post
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        published = Status.objects.get(name="published")
+        context["post_list"] = Post.objects.filter(
+            status=published
+        ).order_by("created_on").reverse()
+        return context
 
+class DraftPostListView(LoginRequiredMixin, ListView):
+    template_name = "posts/list.html"
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        draft = Status.objects.get(name="draft")
+        context["post_list"] = Post.objects.filter(
+            status=draft
+        ).filter(
+            author=self.request.user
+        ).order_by("created_on").reverse()
+        return context
+    
 class PostDetailView(DetailView):
     template_name = "posts/detail.html"
     model = Post
 
-class PostUpdateView(UpdateView):
-    template_name = "posts/update.html"
-    model = Post
-    fields = ["title", "author", "subtitle", "body"]
-
-class PostDeleteView(DeleteView):
-    template_name = "posts/delete.html"
-    model = Post
-    success_url = reverse_lazy("list")
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        draft = Status.objects.get(name="draft")
+        if (
+            context["post"].status == draft 
+            and context["post"].author != self.request.user):
+            context["post"] = None
+        if context["post"]:
+            return context
+        raise PermissionDenied()
     
+class ArchivedPostListView(LoginRequiredMixin, ListView):
+    template_name = "posts/list.html"
+    model = Post
 
-# The UpdateView class requires, at a minimum:
-# 1. template_name
-# 2. model
-# 3. A list of fields that should be present on the generated form
-
-# The update view will require a very specific url pattern which should include
-# the primary key of the instance of our model that we wish to modify.
-
-# The delete view will also require a primary key to be provided through a url pattern
-
-# The delete view will need, at a minimum:
-# 1. template_name
-# 2. model
-# 3. success_url (this should redirect our users after a successful delete).
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        archived = Status.objects.get(name="archived")
+        context["post_list"] = Post.objects.filter(
+            status=archived
+        ).filter(
+            author=self.request.user
+        ).order_by("created_on").reverse()
+        return context
